4b98b51a64462770ad49f6283e12e6ac5b3b91d8
MiriICO26 <miriort.23@gmail.com>
2022-10-17 18:49:52 -0500

    Ya quedó tú :))

---
+++ src/Ejercicios/MarcoCasillaVerificacion/TestCheckBox.java
@@ -0,0 +1,13 @@
+package Ejercicios.MarcoCasillaVerificacion;
+// Fig. 12.18: CheckBoxTest.java
+// CheckBoxFrame test.
+import javax.swing.JFrame;
+
+public class TestCheckBox {
+    public static void main(String[] args){
+        FrameCheckBox checkBoxFrame = new FrameCheckBox();
+        checkBoxFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        checkBoxFrame.setSize(275, 100);
+        checkBoxFrame.setVisible(true);
+    }
+} // end class CheckBoxTest
