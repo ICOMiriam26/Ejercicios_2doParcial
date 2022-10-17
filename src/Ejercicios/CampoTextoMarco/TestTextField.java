f1704e0ab5ca2db6d2564946c83be42cac99603d
MiriICO26 <miriort.23@gmail.com>
2022-10-17 17:19:38 -0500

    Ya quedó tú :))

---
+++ src/Ejercicios/CampoTextoMarco/TestTextField.java
@@ -0,0 +1,13 @@
+package Ejercicios.CampoTextoMarco;
+
+import javax.swing.*;
+
+public class TestTextField {
+    public static void main(String[] args) {
+        FieldTextFrame  frameTextField= new FieldTextFrame();
+        frameTextField.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameTextField.setSize(350, 100);
+        frameTextField.setVisible(true);
+    }
+} // end class TextFieldTest
+
