9d5bfca9fdb2468698c16c977cb4a291669adafa
MiriICO26 <miriort.23@gmail.com>
2022-10-17 17:49:28 -0500

    Ya quedó tú :))

---
+++ src/Ejercicios/MarcoBorderLayout/DemoBorderLayout.java
@@ -0,0 +1,14 @@
+package Ejercicios.MarcoBorderLayout;
+// Fig. 12.42: DemoBorderLayout.java
+// Test of FrameBorderLayout.
+import javax.swing.JFrame;
+
+public class DemoBorderLayout {
+    public static void main(String[] args) {
+        BorderLayoutFramework borderLayoutFramework= new BorderLayoutFramework();
+        borderLayoutFramework.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        borderLayoutFramework.setSize(300, 200);
+        borderLayoutFramework.setVisible(true);
+    }
+} // end of DemoBorderLayout class
+
