6a83064f9e0b0810b5ff47f4ac228c5cb305b021
MiriICO26 <miriort.23@gmail.com>
2022-10-17 18:57:54 -0500

    Ya quedó tú :))

---
+++ src/Ejercicios/MarcoCuadroCombinado/ComboBoxTest.java
@@ -0,0 +1,13 @@
+package Ejercicios.MarcoCuadroCombinado;
+// Fig. 12.22: ComboBoxTest.java
+// ComboFrameTest.
+import javax.swing.JFrame;
+
+public class ComboBoxTest {
+    public static void main(String[] args) {
+        FramesBoxCombined frameBoxCombined = new FramesBoxCombined();
+        frameBoxCombined.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameBoxCombined.setSize(350, 150);
+        frameBoxCombined.setVisible(true);
+    }
+} // end ComboBoxTest class
